64104453f191c655e6abd3c114c9a21b7625c018
Nate Cottle <cottle.nate@gmail.com>
2018-09-14 09:29:35 -0700

    Add authorization for admin route, add dynamic navbar

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,5 @@
 class AdminsController < ApplicationController
+  before_action :authorize_admin
   def index
 
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def authorize_admin
+    if !current_user || current_user.admin != true 
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end
 end
