eded1032b4cdef56553b93c3f5324c17b3d63039
Nate Cottle <cottle.nate@gmail.com>
2018-09-14 11:25:04 -0700

    Start working on styling admin portal, start building seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Skill.destroy_all
+Project.destroy_all
+Hobby.destroy_all
+
+10.times do
+  Skill.create!
+end
